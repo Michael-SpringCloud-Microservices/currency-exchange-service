e4fe0fcc6f19111c249ab07888ce61f63ceb5c0b
MICHAEL PHILOMI <MichaelPhilomi.PaneerSelvam@merrillcorp.com>
2019-02-26 18:33:02 +0530

    Adding the latest changes

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
+//import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/repository/ExchangeValueRepository.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/repository/ExchangeValueRepository.java
@@ -1,6 +1,7 @@
 package com.in28minutes.microservices.currencyexchangeservice.repository;
 
 import com.in28minutes.microservices.currencyexchangeservice.model.ExchangeValue;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExchangeValueRepository extends JpaRepository<ExchangeValue,Long>{
